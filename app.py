ee94c9a58188397a65aaf11819fc9dc8a8f4d653
Connor Cozad <43727978+23ccozad@users.noreply.github.com>
2021-04-28 20:42:24 -0400

    Extended range of graph to include summer 2021

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def configure_rangeslider():
         return dict(
             visible=True,
             bgcolor=LIGHT_GRAY.color_to_str(alpha=0.2),
-            range=[datetime.datetime(2020, 1, 8), datetime.datetime(2021, 4, 30)]
+            range=[datetime.datetime(2020, 1, 8), datetime.datetime(2021, 8, 23)]
         )
     else:
         return dict(visible=False)
@@ -205,7 +205,7 @@ def generate_fig(show_downtown_cases=False, show_county_cases=False, show_county
                 ),
                 rangeslider=configure_rangeslider(),
                 type='date',
-                range=[datetime.datetime(2020, 8, 25), datetime.datetime(2021, 4, 30)]
+                range=[datetime.datetime(2020, 8, 25), datetime.datetime(2021, 8, 23)]
             ),
             yaxis=dict(
                 ticks='outside',
